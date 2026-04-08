cef846b19dd72c9a3ab144ea167bc848aa7f3d04
just-spafi <63868515+just-spafi@users.noreply.github.com>
2026-04-08 16:04:34 +0200

    Fix username in upgrade script

--- keycloak/upgrade-db.sh
+++ keycloak/upgrade-db.sh
@@ -2,10 +2,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 mkdir -p upgrade
-docker compose exec db pg_dumpall -U ephios > ./upgrade/sqldump.sql
+docker compose exec db pg_dumpall -U keycloak > ./upgrade/sqldump.sql
 
 docker compose down -v
 
 docker compose pull && docker compose up --no-start && docker compose start db
 docker compose cp upgrade/sqldump.sql db:/tmp
-docker compose exec db psql -U ephios -f /tmp/sqldump.sql
+docker compose exec db psql -U keycloak -f /tmp/sqldump.sql
