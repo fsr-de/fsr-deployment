9ba346a4a44a491354b0194417243d63b9fee04e
just-spafi <63868515+just-spafi@users.noreply.github.com>
2026-04-08 09:56:20 +0200

    Fix upgrade script

--- ephios/upgrade-db.sh
+++ ephios/upgrade-db.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 mkdir -p upgrade
-docker compose exec db pg_dumpall ephios -U ephios > ./upgrade/sqldump.sql
+docker compose exec db pg_dumpall -U ephios > ./upgrade/sqldump.sql
 
 docker compose pull && docker compose up --no-start && docker compose start db
 docker compose cp upgrade/sqldump.sql db:/tmp
