248e781b945ef802ef118a400c2a50e2cac0c967
just-spafi <63868515+just-spafi@users.noreply.github.com>
2026-04-08 16:25:19 +0200

    Update Keycloak to 26.6.0

--- keycloak/upgrade-db.sh
+++ keycloak/upgrade-db.sh
@@ -8,4 +8,6 @@ docker compose down -v
 
 docker compose pull && docker compose up --no-start && docker compose start db
 docker compose cp upgrade/sqldump.sql db:/tmp
+
+sleep 5 # Wait 5 seconds for the container to start
 docker compose exec db psql -U keycloak -f /tmp/sqldump.sql
